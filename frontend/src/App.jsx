352f45070fe26d38b537972590f08a29f9336465
antonin-san <antonin@kanji.io>
2022-06-23 18:40:25 +0200

    adding file hashing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,131 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import axios from "axios";
+import SparkMD5 from "spark-md5";
+import Q from "q";
+import React, { Component } from "react";
+class App extends Component {
+   calculateMD5Hash(file, bufferSize) {
+    var def = Q.defer();
+  
+    var fileReader = new FileReader();
+    var fileSlicer = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice;
+    var hashAlgorithm = new SparkMD5();
+    var totalParts = Math.ceil(file.size / bufferSize);
+    var currentPart = 0;
+    var startTime = new Date().getTime();
+  
+    fileReader.onload = function(e) {
+      currentPart += 1;
+  
+      def.notify({
+        currentPart: currentPart,
+        totalParts: totalParts
+      });
+  
+      var buffer = e.target.result;
+      hashAlgorithm.appendBinary(buffer);
+  
+      if (currentPart < totalParts) {
+        processNextPart();
+        return;
+      }
+  
+      def.resolve({
+        hashResult: hashAlgorithm.end(),
+        duration: new Date().getTime() - startTime
+      });
+    };
+  
+    fileReader.onerror = function(e) {
+      def.reject(e);
+    };
+  
+    function processNextPart() {
+      var start = currentPart * bufferSize;
+      var end = Math.min(start + bufferSize, file.size);
+      fileReader.readAsBinaryString(fileSlicer.call(file, start, end));
+    }
+  
+    processNextPart();
+    return def.promise;
+  }
+     
+  state = {
+    // Initially, no file is selected
+    selectedFile: null,
+    fileHash: "0x00"
+  };
 
-function App() {
-  const [count, setCount] = useState(0)
+  // On file select (from the pop up)
+  onFileChange = (event) => {
+    // Update the state
+    this.setState({ selectedFile: event.target.files[0] });
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  // On file upload (click the upload button)
+  onFileUpload = () => {
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+      "file",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+
+    axios.post("http://localhost:8080/upload", formData);
+  };
+
+  // File content to be displayed after
+  // file upload is complete
+  fileData = () => {
+
+    if (this.state.selectedFile) {
+      var file = this.state.selectedFile;
+      var bufferSize = Math.pow(1024, 2) * 10; // 10MB
+    
+      this.calculateMD5Hash(file, bufferSize).then(res => {
+      this.setState({ ...this.state, fileHash: res.hashResult });
+    })
+
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {this.state.selectedFile.name}</p>
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>File Hash: {this.state.fileHash}</p>
+          <p>
+            Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Upload</h1>
+        <h3>File Upload using React!</h3>
+        <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>Upload!</button>
+        </div>
+        {this.fileData()}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
